908591a836b3198e49174eb3a49fba5a61fa6535
zscnbbdbd <150670099+zscnbbdbd@users.noreply.github.com>
2026-07-10 14:28:31 +0800

    Add backend/src/main/java/com/smartimage/ImageAnalyzerApplication.java

---
+++ backend/src/main/java/com/smartimage/ImageAnalyzerApplication.java
@@ -0,0 +1,11 @@
+package com.smartimage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageAnalyzerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ImageAnalyzerApplication.class, args);
+    }
+}
